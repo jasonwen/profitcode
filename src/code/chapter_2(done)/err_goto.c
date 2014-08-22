e5e7896c61f873cde9bd021c1b57d33985eb2721
jasonwen <wenjx2009@gmail.com>
2014-08-22 20:47:28 +0800

    Update err_goto.c
    添加注释

--- src/code/chapter_2(done)/err_goto.c
+++ src/code/chapter_2(done)/err_goto.c
@@ -1,3 +1,9 @@
+/*
+用途：把文件中的ascii大写字母转换为小写字母
+重点：stat的使用，fopen的使用，goto的使用
+操作：读文件状态信息、打开关闭文件、读文件内容
+*/
+
 #include <stdio.h>
 
 int convert(void)
@@ -8,30 +14,36 @@ int convert(void)
 	int n;
 	int res = -1;
 
+        //获取文件的状态信息，包括inode、文件大小和创建时间等
 	if(stat("test.txt", &statbuf) == -1){
 		perror("fail to get stat");
 		return res;
 	}
 
-	fp = fopen("test.txt", "rb");
+       //打开文件
+	fp = fopen("test.txt", "rb"); //"b" mode是为了兼容C89标准，在linux下没有
+                                                //作用，在其他系统可能用于标明二进制文件
 	if(fp == NULL){
 		perror("fail to open");
 		return res;
 	}
 	
+        //动态分配内存
 	p = (char *)malloc(sizeof(char) * (statbuf.st_size + 1));
 	if(p == NULL){
 		perror("fail to malloc");
-		goto err1;
+		goto err1; //分配内存失败，跳到err1 label
 	}
 	
+        //读取文件内容
 	n = fread(p, sizeof(char), statbuf.st_size, fp);
 	if(n == -1){
 		perror("fail to read");
-		goto err2;
+		goto err2;  //读取文件失败，跳到err2 label
 	}
 	*(p + n) = '\0';
-	
+
+        //执行大写字母转换为小写字母的操作	
 	q = p;
 	while(*q != '\0'){
 		if('a' =< *q && *q <= 'z')
