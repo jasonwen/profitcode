c638db67c41634577b19d199f1be1bf4e67677ac
jasonwen <wenjx2009@gmail.com>
2014-08-22 20:26:59 +0800

    Update do_while.c
    添加注释

--- src/code/chapter_2(done)/do_while.c
+++ src/code/chapter_2(done)/do_while.c
@@ -1,20 +1,24 @@
+/*
+用途：计算n的阶乘，n由用户输入
+重点：scanf的使用，do while的使用
+*/
 #include <stdio.h>
 
 int main(void)
 {
 	int n;
-	int mul = 1;
+	int result = 1;
 	int i;
 
-	scanf("%d", &n);
+	scanf("%d", &n); //从标准输入中读取n的值
 	
 	i = 1;
 	do{
-		mul *= i;
+		result *= i;
 		i++;
 	}while(i <= n);
 
-	printf("the result is %d\n", mul);
+	printf("the result is %d\n", result);
 
 	return 0;
 }
